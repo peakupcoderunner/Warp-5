f71679e42324837f354a8abe96ef30386ca4754f
CodeRunner <44100023+peakupcoderunner@users.noreply.github.com>
2018-10-26 23:55:16 +0300

    Add files via upload

---
+++ likeitClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace likeitClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                var returnedValue = client.UploadString("https://j1csq3ukgg.execute-api.us-east-2.amazonaws.com/dev/", "\"peakup\"");
+                Console.WriteLine(returnedValue);
+            }
+            Console.ReadLine();
+        }
+    }
+}
